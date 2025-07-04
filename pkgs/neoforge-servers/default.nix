1c57087bf9dc45e75d517f26ae934fa200f36880
Nathan Regner <nathanregner@gmail.com>
2025-07-04 09:28:44 -0600

    neoforge-servers: init
    Builts upon a lot of the work from issue 15.
    
    This approach extracts required libraries from the installer jar, so we
    don't have to worry about installer non-determinism breaking a FOD. It's
    a little hacky, but doesn't feel too bad.
    
    Only supports `20.4.240+`, as earlier versions don't support the "fat
    jar" functionality that allows us to prefetch server mappings.

---
+++ pkgs/neoforge-servers/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  callPackage,
+  vanillaServers,
+}:
+let
+  inherit (lib.our) escapeVersion;
+  inherit (lib)
+    nameValuePair
+    flatten
+    last
+    versionOlder
+    mapAttrsToList
+    versions
+    ;
+
+  sortBy = attr: f: builtins.sort (a: b: f a.${attr} b.${attr});
+
+  loaderLocks = lib.importJSON ./loader_locks.json;
+  libraryLocks = lib.importJSON ../build-support/libraries.json;
+  gameLocks = lib.importJSON ./game_locks.json;
+
+  packages = mapAttrsToList (
+    gameVersion: builds:
+    sortBy "version" versionOlder (
+      mapAttrsToList (
+        buildVersion: build:
+        callPackage ./derivation.nix {
+          inherit libraryLocks;
+          build = build // {
+            version = buildVersion;
+          };
+          gameVersion = gameLocks.${gameVersion} // {
+            version = gameVersion;
+          };
+          minecraft-server = vanillaServers."vanilla-${escapeVersion gameVersion}";
+        }
+      ) builds
+    )
+  ) loaderLocks;
+
+  # Latest build for each MC version
+  latestBuilds = sortBy "version" versionOlder (map last packages);
+in
+lib.recurseIntoAttrs (
+  builtins.listToAttrs (
+    (map (x: nameValuePair (escapeVersion x.name) x) (flatten packages))
+    ++ (map (
+      x: nameValuePair "${x.pname}-${escapeVersion (versions.majorMinor x.version)}" x
+    ) latestBuilds)
+    ++ [ (nameValuePair "neoforge" (last latestBuilds)) ]
+  )
+)
